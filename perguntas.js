96dce673e7ceaabe9f0669f87776783a8a116280
manuellamm <00001119642851sp@al.educacao.sp.gov.br>
2024-11-22 11:14:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um DER?',
+    'DER ou Diagrama de Entidade Relacionamento é um termo que se refere ao fluxograma de funcionamento de um banco de dados, pontuando suas características diversas. Foi feito para se ter noção dos processos do banco'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um dado?',
+    'Um dado é basicamente uma informação arquivada, ela tendo respectivamente sua unidade de medição  e informações complementares'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz vaca em Inglês?',
+    'Vaca em ingles é COW (cáu)'
+)
